8100ae3a4aae5604b7c534a4e5f063ef41fd422b
tomer kakou <tomerka3@ac.sce.ac.il>
2024-04-10 13:26:41 +0300

    fix category name empty

--- ComputerNetworksProject/Data/Category.cs
+++ ComputerNetworksProject/Data/Category.cs
@@ -5,6 +5,7 @@ namespace ComputerNetworksProject.Data
     public class Category
     {
         [Key]
+        [StringLength(40,MinimumLength =1,ErrorMessage ="Category name cannot be empty!")]
         public string Name { get; set; }
 
     }
